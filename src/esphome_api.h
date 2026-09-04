31f6a1e33557ab8c4dc68a6dfb797ca955421348
hamza özkan <hmz_06_1996@hotmail.com>
2026-09-05 00:03:08 +0300

    Refactor ESPHomeAPI class methods and formatting
    Removed redundant method declarations and adjusted formatting for clarity.

--- src/esphome_api.h
+++ src/esphome_api.h
@@ -3,8 +3,8 @@
 #include <Arduino.h>
 #include <WiFi.h>
 
-class ESPHomeAPI
-{
+class ESPHomeAPI {
+
 public:
 
     ESPHomeAPI();
@@ -21,71 +21,25 @@ private:
     bool subscribed = false;
 
     void handleClient();
-
     void resetConnectionState();
+    bool readFrame(uint8_t &messageType, uint8_t *data,uint16_t &length);
+    void processMessage(uint8_t messageType, uint8_t *data, uint16_t length);
+    void sendFrame(uint8_t messageType, const uint8_t *data, uint16_t length);
 
-    bool readFrame(
-        uint8_t &messageType,
-        uint8_t *data,
-        uint16_t &length
-    );
-
-    void processMessage(
-        uint8_t messageType,
-        uint8_t *data,
-        uint16_t length
-    );
-
-    void sendFrame(
-        uint8_t messageType,
-        const uint8_t *data,
-        uint16_t length
-    );
-
-    void handleHello(
-        const uint8_t *data,
-        uint16_t length
-    );
+    void handleHello(const uint8_t *data, uint16_t length);
 
     void handleDeviceInfo();
-
     void handleListEntities();
-
     void handleSubscribeStates();
-
-    void handleLightCommand(
-        const uint8_t *data,
-        uint16_t length
-    );
-
+    void handleLightCommand(const uint8_t *data, uint16_t length);
     void sendLightState();
 
-    uint32_t readFixed32(
-        const uint8_t *data
-    );
-
-    float readFloat(
-        const uint8_t *data
-    );
-
-    void writeFixed32(
-        uint8_t *data,
-        uint32_t value
-    );
-
-    void writeFloat(
-        uint8_t *data,
-        float value
-    );
-
-    uint8_t encodeVarint(
-        uint8_t *buffer,
-        uint32_t value
-    );
-
-    uint8_t encodeString(
-        uint8_t *buffer,
-        uint8_t field,
-        const char *str
-    );
+    uint32_t readFixed32(const uint8_t *data);
+    float readFloat(const uint8_t *data);
+    void writeFixed32(uint8_t *data, uint32_t value);
+    void writeFloat(uint8_t *data, float value);
+    uint8_t encodeVarint(uint8_t *buffer, uint32_t value);
+    uint8_t encodeString(uint8_t *buffer, uint8_t field, const char *str);
 };
+
+
